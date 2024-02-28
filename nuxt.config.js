4f67ef5bcc49bb5724050f752ba5bdc020a815cd
agustin.capeletto <agustin.capeletto@leniolabs.com>
2024-02-28 08:19:41 -0300

    init

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,7 +31,9 @@ export default {
       },
     ],
   },
-
+  publicRuntimeConfig: {
+    myVariable: process.env.OPENAI,
+  },
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [{ src: "~/plugins/globalObject.js" }],
 
